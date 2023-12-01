02436de4ab785b76dc681eadde511120044e563a
chellshelove <chellshelove733@gmail.com>
2023-12-02 01:20:24 +0700

    test

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+
+import { Footer, NavBar } from "../components"; // Update the import path to the correct location of the components
+
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover world's best car showcase application",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className='relative'>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
